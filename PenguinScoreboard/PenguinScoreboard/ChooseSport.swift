3716ad720b8877dc7c15ea4c5257384bf00a3587
TrevorToh <145330041+TrevorToh@users.noreply.github.com>
2024-06-24 15:52:49 +0800

    made the views

---
+++ PenguinScoreboard/PenguinScoreboard/ChooseSport.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseSport.swift
+//  PenguinScoreboard
+//
+//  Created by Trevor Toh Hong Rui on 24/6/24.
+//
+
+import SwiftUI
+
+struct ChooseSport: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseSport()
+}
---
+++ PenguinScoreboard/PenguinScoreboard/ScoreboardView.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreboardView.swift
+//  PenguinScoreboard
+//
+//  Created by Trevor Toh Hong Rui on 24/6/24.
+//
+
+import SwiftUI
+
+struct ScoreboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScoreboardView()
+}
