4d34d201cd2ced035bf1b79dd7e3d8f2205be966
Kyrans-Macbook-Air <kyran_tan_kai_loon@s2023.ssts.edu.sg>
2024-06-24 15:43:48 +0800

    Initial commit

---
+++ PenguinScoreboard/PenguinScoreboard/PenguinScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenguinScoreboardApp.swift
+//  PenguinScoreboard
+//
+//  Created by KR on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PenguinScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
